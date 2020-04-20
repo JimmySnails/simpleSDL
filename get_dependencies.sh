a989c681a0272366a36b31475fa0ccd69aa2f8e0
JimmySnails <talantyyr@gmail.com>
2020-04-20 11:51:39 +0200

    Add SDL2 Mixer and TTF

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -2,18 +2,28 @@
 
 pushd contrib
 
-wget https://www.libsdl.org/release/SDL2-2.0.9.zip
-unzip -q SDL2-2.0.9.zip
+wget https://www.libsdl.org/release/SDL2-2.0.9.tar.gz
 rm -rf SDL2
+tar xf SDL2-2.0.9.tar.gz
 mv SDL2-2.0.9 SDL2
-rm SDL2-2.0.9.zip
+rm SDL2-2.0.9.tar.gz
 
-wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.zip
-unzip -q SDL2_image-2.0.5.zip
+wget https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.tar.gz
 rm -rf SDL2_image
+tar xf SDL2_image-2.0.5.tar.gz
 mv SDL2_image-2.0.5 SDL2_image
-rm SDL2_image-2.0.5.zip
+rm SDL2_image-2.0.5.tar.gz
+
+wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz
+rm -rf SDL2_mixer
+tar xf SDL2_mixer-2.0.4.tar.gz
+mv SDL2_mixer-2.0.4 SDL2_mixer
+rm SDL2_mixer-2.0.4.tar.gz
+
+wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.tar.gz
+rm -rf SDL2_ttf
+tar xf SDL2_ttf-2.0.15.tar.gz
+mv SDL2_ttf-2.0.15 SDL2_ttf
+rm SDL2_ttf-2.0.15.tar.gz
 
 popd
-# No need to copy SDLActivity.java et al, repo contains those from 2.0.8
-# cp external/SDL2/SDL2/android-project/app/src/main/java/org/libsdl/app/*.java android/app/src/main/java/org/libsdl/app/
--- source/main.c
+++ source/main.c
@@ -1,6 +1,8 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
 #if defined(EMSCRIPTEN)
 #include <emscripten.h>
